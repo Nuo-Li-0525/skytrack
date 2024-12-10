11d0e998284903b1d1ec98e2dce409f48b5e8712
Kenny <prizekenny@gmail.com>
2024-12-10 00:07:47 -0700

    Import Suspense

--- app/city/page.js
+++ app/city/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 import { getWeather } from "../api/weather";
